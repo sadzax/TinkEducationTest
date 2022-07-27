3cf2825223ae0f93a8e17078c6fd4c465526bce2
Marat Ramazanov <sadzax@gmail.com>
2022-07-27 16:37:41 +0300

    v.0.3.

--- task06.py
+++ task06.py
@@ -1,7 +1,7 @@
 # Mars hours = h, minutes = m, work_time = k, work_schedule_periods_starts_at = m/2 (performs twice an hour)
 # n = trains departure every day in their strict time schedule
 # Goal: find t = time of end of 1st work in hour
-# Unconvinient = Train that departures within work_time
+# Inconvenient = Train that departures within work_time
 
 error = 'Please enter correct integers: trains up to 10"5", hours in day up to 10^9, minutes in hour: evens ' \
         'up to 10^9; work time: up to half of the minutes in hour, departure hour and minutes:' \
@@ -32,13 +32,13 @@ while True:
         continue
 k = inputter_int('How many minutes required for the work: ', error, 1, m/2, 0)
 
-trains = {0:[0,0]}
+trains = {}
 i = 0
 while i < n:
     dep_i = int(i+1)
     dep_h_desc = f"Train # {i + 1} departure hour: "
-    dep_m_desc = f"Train # {i + 1} departure minute: "
     dep_h = inputter_int(str(dep_h_desc), error, 0, h, h)
+    dep_m_desc = f"Train # {i + 1} departure minute: "
     dep_m = inputter_int(str(dep_m_desc), error, 0, m, m)
     trains[dep_i] = [dep_h, dep_m]
     i = i + 1
@@ -52,22 +52,23 @@ def work_timing(i):
         work_time_start[1] = int(work_time_start[1] - m / 2)
         work_time_start[2] = int(work_time_start[2] - m / 2)
     return work_time_start
-
 def find_out_busy_munutes(j):
     corr_time_start = work_timing(j)
-    busy_minutes = [x for x in range(corr_time_start[1] + 1, corr_time_start[1] + k - 1, 1)] + \
-                   [x for x in range(corr_time_start[2] + 1, corr_time_start[2] + k - 1, 1)]
+    busy_minutes = [x for x in range(corr_time_start[1] + 1, corr_time_start[1] + k, 1)] + \
+                   [x for x in range(corr_time_start[2] + 1, corr_time_start[2] + k, 1)]
     for minute in busy_minutes:
         if minute >= m:
             busy_minutes[busy_minutes.index(minute)] = minute - m
     return busy_minutes
 
-x = find_out_busy_munutes(0)
+inconvenient_trains = []
 
-for i in trains:
-    if i != 0:
-        for minute in x:
-            if minute == trains.get(i)[1]:
-                print(trains.get(i)[0])
-    else:
-        continue
+for first_work_starts_minute in range(0,m+1,1):
+    print(first_work_starts_minute)
+    x = find_out_busy_munutes(first_work_starts_minute)
+    print(x)
+    for i in range(1,n+1,1):
+        for work_minute in x:
+            if trains.get(i)[1] == work_minute:
+                print(i)
+    print(f"That's it \n\n")
